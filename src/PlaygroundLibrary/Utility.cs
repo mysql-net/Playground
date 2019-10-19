e450fea198e64f1101078b2a5d2e2fe0409f2e58
Bradley Grainger <bgrainger@gmail.com>
2019-10-18 22:12:32 -0700

    Add simple project.

---
+++ src/PlaygroundLibrary/Utility.cs
@@ -0,0 +1,12 @@
+﻿namespace PlaygroundLibrary
+{
+	public static class Utility
+	{
+		public static int GetRandomNumber()
+		{
+			// chosen by fair dice roll.
+			// guaranteed to be random.
+			return 4;
+		}
+	}
+}
